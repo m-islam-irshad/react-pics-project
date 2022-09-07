50598f9502003a7d9299318abc028afc25678c32
mi-ibne-irshad <islambinirshad@gmail.com>
2022-09-07 16:44:11 +0500

    Pics Project now complete sementic ui import and form handeling events done

---
+++ src/Components/SearchBar.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+class SearchBar extends React.Component{
+
+    onInputChange(event){
+       console.log(event.target.value)
+    }
+
+    // onInputClick(){
+    //     console.log("On Input clicked")
+    // }
+
+    render(){
+        return(
+            <div className="ui segment">
+                <form className="ui form">
+                <div className="field">
+                    <label>Image Search</label>
+                    <input type="text" onChange={this.onInputChange} />
+                    </div>
+                </form>
+
+            </div>
+        )
+    }
+}
+
+
+
+export default SearchBar;
